5dbb2934e59849c2ae8fee8859ba4479d74ec8fb
Mohamed LOTF <mohamed.lotf@outlook.com>
2021-01-03 21:57:47 +0100

    add all blog project files

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2021_01_03_203218) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
